f58191cd9af41dc69173206ce0dd2f7c04bd8551
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2025-08-10 09:42:46 +0530

    protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,34 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { Provider } from "react-redux";
+import appStore from "./utils/store";
+
 import Body from "./components/Body";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
-import { Provider } from "react-redux";
-import AppStore from "./utils/store";
-import appStore from "./utils/store";
 import Feed from "./components/Feed";
+import ProtectedRoute from "./components/ProtectRoute";
+
 const App = () => {
   return (
-    <>
-      <Provider store={appStore}>
-        <BrowserRouter basename="/">
-          <Routes>
-            <Route path="/" element={<Body />}>
+    <Provider store={appStore}>
+      <BrowserRouter>
+        <Routes>
+          {/* Public route */}
+          <Route path="/login" element={<Login />} />
+
+          {/* Everything else is protected */}
+          <Route element={<ProtectedRoute />}>
+            <Route element={<Body />}>
               <Route path="/" element={<Feed />} />
-              <Route path="/login" element={<Login />} />
+              <Route path="/feed" element={<Feed />} />
               <Route path="/profile" element={<Profile />} />
+              {/* catch-all for any other route */}
+              <Route path="*" element={<Feed />} />
             </Route>
-          </Routes>
-        </BrowserRouter>
-      </Provider>
-    </>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,136 +1,66 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
-import { useNavigate } from "react-router-dom";
-import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
-  const [emailId, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [isLoginFrom, setIsLoginForm] = useState(true);
-  const [error, setError] = useState("");
+  const [emailId, setEmailId] = useState("arun@gmail.com");
+  const [password, setPassword] = useState("Arun@123");
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
       const res = await axios.post(
-        BASE_URL + "/login",
+        "http://localhost:9999/login",
         {
           emailId,
           password,
         },
         { withCredentials: true }
       );
-      // console.log(res.data.user)
-      dispatch(addUser(res.data.user));
-      return navigate("/");
+      console.log(res.data);
+      dispatch(addUser(res?.data));
+      navigate("/profile");
     } catch (err) {
-      setError(err.response.data);
       console.log(err);
     }
   };
 
-  const handleSignUp = async () => {
-    try {
-      const res = await axios.post(
-        BASE_URL + "/signup",
-        {
-          firstName,
-          lastName,
-          emailId,
-          password,
-        },
-        {
-          withCredentials: true,
-        }
-      );
-      console.log(res);
-      dispatch(addUser(res.data.data));
-      return navigate("/profile");
-    } catch (error) {
-      setError(error.response.data);
-      console.log(error);
-    }
-  };
-
   return (
-    <div className="flex justify-center my-10">
+    <div className="flex justify-center my-25">
       <div className="card bg-base-300 w-96 shadow-xl">
         <div className="card-body">
-          <h2 className="card-title justify-center">
-            {isLoginFrom ? "Login" : "Signup"}
-          </h2>
+          <h2 className="card-title justify-center">Login</h2>
           <div>
-            {!isLoginFrom && (
-              <>
-                <label className="form-control w-full max-w-xs my-2">
-                  <div className="label">
-                    <span className="label-text">Firstname</span>
-                  </div>
-                  <input
-                    type="text"
-                    value={firstName}
-                    onChange={(e) => setFirstName(e.target.value)}
-                    className="input input-bordered w-full max-w-xs"
-                  />
-                </label>
-                <label className="form-control w-full max-w-xs my-2">
-                  <div className="label">
-                    <span className="label-text">Lastname</span>
-                  </div>
-                  <input
-                    type="text"
-                    value={lastName}
-                    onChange={(e) => setLastName(e.target.value)}
-                    className="input input-bordered w-full max-w-xs"
-                  />
-                </label>
-              </>
-            )}
-            <label className="form-control w-full max-w-xs my-2">
-              <div className="label">
-                <span className="label-text">Email</span>
-              </div>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Email</legend>
               <input
                 type="text"
+                className="input"
                 value={emailId}
-                onChange={(e) => setEmail(e.target.value)}
-                className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setEmailId(e.target.value)}
               />
-            </label>
-            <label className="form-control w-full max-w-xs my-2">
-              <div className="label">
-                <span className="label-text">Password</span>
-              </div>
+            </fieldset>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Password</legend>
               <input
-                type="password"
+                type="text"
+                className="input"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                className="input input-bordered w-full max-w-xs"
               />
-            </label>
+            </fieldset>
           </div>
-          <p className="text-red-500 text-center">{error}</p>
-          <div className="card-actions justify-center mt-2">
+          <div className="card-actions justify-center">
             <button
-              className="btn btn-primary"
-              onClick={isLoginFrom ? handleLogin : handleSignUp}
+              className="btn btn-primary rounded-lg my-5"
+              onClick={handleLogin}
             >
-              {isLoginFrom ? "Login" : "Signup"}
+              Login
             </button>
           </div>
-          <p
-            className=" text-center cursor-pointer py-2"
-            onClick={() => setIsLoginForm((value) => !value)}
-          >
-            {isLoginFrom
-              ? "New user ? signup here"
-              : "Existing User ? Login here"}
-          </p>
         </div>
       </div>
     </div>
